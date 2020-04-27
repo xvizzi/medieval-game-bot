0801270bdaa7ffabe94b6bea10a6bb23f6e8ab4e
xvizzi <xvizzi1@gmail.com>
2020-04-27 16:25:19 -0400

    temp code working?

--- bot.js
+++ bot.js
@@ -1,22 +1,13 @@
-var Discord = require('discord.io');
-var logger = require('winston');
+var Discord = require('discord.js');
 var auth = require('./auth.json')
-
-//create the logger settings
-logger.remove(logger.transports.Console);
-logger.add(new logger.transports.Console, {colorize:true});
-
-logger.level = 'debug';
 
 var bot = new Discord.Client({
     token: auth.token,
     autorun: true
 });
 bot.on('ready', function(evt) {
-    logger.info('Connected');
-    logger.info('Logged in as: ');
-    logger.info(bot.username + ' - (' + bot.id + ')');
-});
+    console.log('Connected as:'  + bot.username + ' - (' + bot.id + ')' )
+}); 
 
 bot.on('message', function(user,userID,channelID,message,evt) {
     if (message.substring(0,1) == '!') {
