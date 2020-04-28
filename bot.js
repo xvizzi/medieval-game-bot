52ae229078c1e39a2f3af7b9ecd9c4576324f77c
xvizzi <xvizzi1@gmail.com>
2020-04-27 20:16:31 -0400

    created template command

--- bot.js
+++ bot.js
@@ -1,27 +1,27 @@
 var Discord = require('discord.js');
 var auth = require('./auth.json')
 
-var bot = new Discord.Client({
-    token: auth.token,
-    autorun: true
-});
-bot.on('ready', function(evt) {
-    console.log('Connected as:'  + bot.username + ' - (' + bot.id + ')' )
+var bot = new Discord.Client();
+
+bot.on('ready', () => {
+    console.log('Connected as: '  + bot.user.tag)
 }); 
 
-bot.on('message', function(user,userID,channelID,message,evt) {
-    if (message.substring(0,1) == '!') {
-      var argument = message.substring(1).split(' ');
-      var command = argument[0];
-      
-      argument = argument.splice(1);
-      switch (command){
-        case 'ping':
-          bot.sendMessage({
-            to:channelID,
-            message: 'Pong!'
-          });
-          break;
-      }
+bot.on('message', message => {
+  //make sure any command starts with a "!"
+    const prefix = '!';
+    
+    //if the message doesnt have the prefix, leave this block
+    if(!message.content.startsWith(prefix)) return
+    
+    var postfix = message.content.substring(1)
+    
+    switch (postfix) {
+      case "ping": 
+        message.channel.send("pong")
+        break; 
     }
-})
+    
+  })
+
+bot.login(auth.token)
